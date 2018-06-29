c20da829701211bd717f8512cc987a0a092e19bf
Ben Ellentuck <ellentuckben@gmail.com>
2018-06-29 10:52:17 -0400

    long polling up and running!

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 const express = require('express')
-const { EventEmitter } = require('events')
+const app = express()
 
+app.get('/', (req, res) => {
+  res.send(`
+    <html>
+      <head>
+        <script type="text/javascript">
+          console.log('hello world!')
+          function longPollForTime () {
+            fetch('/the-time', { headers: { 'Cache-Control': 'no-cache' } })
+              .then(response => response.text())
+              .then(time => {
+                console.log('The time is: ', time)
+                longPollForTime()
+              })
+          }
+          longPollForTime()
+        </script>
+      </head>
+    </html>
+  `)
+})
+
+const { EventEmitter } = require('events')
 const clock = new EventEmitter()
 setInterval(() => {
   const time = (new Date()).toLocaleString()
@@ -9,19 +31,11 @@ setInterval(() => {
 
 clock.on('tick', time => console.log('The time is ', time))
 
-const app = express()
-app.get('/', (req, res) => {
-  res.send(`
-    <html>
-      <head>
-        <script type="text/javascript">
-          console.log('hello world!')
-        </script>
-      </head>
-    </html>
-  `)
+app.get('/the-time', (req, res) => {
+  clock.once('tick', time => res.send(time))
 })
 
 app.listen(3333, () => {
   console.log('listening on 3333...')
 })
+
